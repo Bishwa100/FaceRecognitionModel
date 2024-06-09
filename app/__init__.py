df8db2ec4112f7c18651687995e1c18ba0f23345
Bishwanath Jana <bishwanathjanab4u@gmail.com>
2024-06-09 09:51:59 +0530

    some env related changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from .utils import load_models
 from .config import get_db_connection
-from .routes import main  # Import the blueprint
-
+from .routes import main  
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
     app.config['DB_CONNECTION'] = get_db_connection()
     
-    # Register the blueprint
     app.register_blueprint(main)
 
     with app.app_context():
