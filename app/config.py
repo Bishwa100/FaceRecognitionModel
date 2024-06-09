5c50ad45543d13365d74a9b67f06cc43d38fa357
Bishwanath Jana <bishwanathjanab4u@gmail.com>
2024-06-09 09:51:59 +0530

    some env related changes

--- app/config.py
+++ app/config.py
@@ -1,15 +1,18 @@
 import os
+from dotenv import load_dotenv
 import psycopg2
+
+load_dotenv()
 
 class Config:
     DEBUG = True
 
 def get_db_connection():
     conn = psycopg2.connect(
-        database=os.getenv('POSTGRES_DB', 'mydatabase'),
-        user=os.getenv('POSTGRES_USER', 'user'),
-        password=os.getenv('POSTGRES_PASSWORD', 'password'),
-        host=os.getenv('POSTGRES_HOST', 'db'),  # Default to 'db'
-        port=os.getenv('POSTGRES_PORT', '5432')  # Default to '5432'
+        database=os.getenv('POSTGRES_DB'),
+        user=os.getenv('POSTGRES_USER'),
+        password=os.getenv('POSTGRES_PASSWORD'),
+        host=os.getenv('POSTGRES_HOST'),  
+        port=os.getenv('POSTGRES_PORT')  
     )
     return conn
